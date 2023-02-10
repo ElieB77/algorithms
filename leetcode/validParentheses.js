1b34d4f2913c9c7ced4c3e6a4f80510958747683
Elie Bissor <eliebiss.26@gmail.com>
2023-02-10 17:41:11 +0100

    validParentheses algo solved.

---
+++ leetcode/validParentheses.js
@@ -0,0 +1,38 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+var isValid = function (s) {
+  let closingBrackets = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      closingBrackets.push(")");
+    } else if (s[i] === "[") {
+      closingBrackets.push("]");
+    } else if (s[i] === "{") {
+      closingBrackets.push("}");
+    } else if (closingBrackets.pop() !== s[i]) {
+      return false;
+    }
+  }
+
+  return closingBrackets.length !== 0 ? false : true;
+};
